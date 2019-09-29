cd3f5b6e017e670cfb3383bd5b4a37cc2fbbac24
Michail Safronov <msaf1980@gmail.com>
2019-09-29 23:49:27 +0500

    cmake

--- prep.sh
+++ prep.sh
@@ -6,6 +6,12 @@ git clone https://github.com/darcamo/conan-cxxopts || exit 1
 cd conan-cxxopts && conan create . local/stable || exit 1
 cd .. && rm -rf conan-cxxopts
 
+git clone https://github.com/msaf1980/conan-libevfibers || exit 1
+cd conan-libevfibers || exit 1
+git checkout 0.4.1-57-g8f0a419 || exit 1
+conan create . local/stable || exit 1
+cd .. && rm -rf conan-libevfibers
+
 #git clone https://github.com/msaf1980/conan-plog || exit 1
 #cd conan-plog && conan create . local/stable || exit 1
 #cd .. && rm -rf conan-plog
